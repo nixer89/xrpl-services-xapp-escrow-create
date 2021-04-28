0aaf2fdbf10166b35a8fed5208452eb85c6bf42d
nixer89 <pbnixer@gmail.com>
2021-04-28 12:53:09 +0200

    v.0.3.4: change expected release date to properly cover escrows created to be released at the minute "0" of an hour. Allow margin of 4 minutes

--- src/app/escrowcreate.ts
+++ src/app/escrowcreate.ts
@@ -810,7 +810,9 @@ export class EscrowCreateComponent implements OnInit, OnDestroy {
         if(expectedRelease.getTime() < Date.now())
             expectedRelease.setTime(Date.now());
 
-        expectedRelease.setHours(expectedRelease.getHours()+1);
+        if(expectedRelease.getMinutes() >= 4 && expectedRelease.getMinutes() <= 59)
+          expectedRelease.setHours(expectedRelease.getHours()+1);
+
         expectedRelease.setMinutes(5,0,0);
         return expectedRelease.toLocaleString();
     } else
