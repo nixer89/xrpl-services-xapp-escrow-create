77aba6c5de166a920e883a9188a7d51e8d8576b6
nixer89 <pbnixer@gmail.com>
2024-06-28 17:34:26 +0200

    v.1.2.1: fix android

--- src/app/escrowcreateXrpl.ts
+++ src/app/escrowcreateXrpl.ts
@@ -492,6 +492,7 @@ export class EscrowCreateComponentXrpl implements OnInit, OnDestroy {
       return new Promise( async (resolve, reject) => {
 
         //use event listeners over websockets
+        //use event listeners over websockets
         if(typeof window.addEventListener === 'function') {
           window.addEventListener("message", event => {
             try {
@@ -504,8 +505,47 @@ export class EscrowCreateComponentXrpl implements OnInit, OnDestroy {
 
                   window.removeAllListeners("message");
 
-                  //add event listeners
-                  window.addEventListener("message", event => this.handleOverlayEvent(event));
+                  if(typeof document.addEventListener === 'function') {
+                    document.removeAllListeners("message");
+                  }
+
+                  if(eventData.reason == "SIGNED") {
+                    //create own response
+                    let message = {
+                      signed: true,
+                      payload_uuidv4: eventData.uuid
+                    }
+                    
+                    resolve(message);
+
+                  } else if(eventData.reason == "DECLINED") {
+                    //user closed without signing
+                    resolve(null)
+                  }
+                }
+              }
+            } catch(err) {
+              //ignore errors
+            }
+          });
+        }
+
+        //use event listeners over websockets
+        if(typeof document.addEventListener === 'function') {
+          document.addEventListener("message", event => {
+            try {
+              let anyEvent:any = event;
+              if(anyEvent && anyEvent.data) {
+                let eventData = JSON.parse(anyEvent.data);
+        
+                console.log("WINDOW: " + eventData);
+
+                if(eventData && eventData.method == "payloadResolved") {
+
+                  document.removeAllListeners("message");
+                  if(typeof window.addEventListener === 'function') {
+                    window.removeAllListeners("message");
+                  }
 
                   if(eventData.reason == "SIGNED") {
                     //create own response
